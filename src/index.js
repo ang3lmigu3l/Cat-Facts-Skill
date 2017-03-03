377e2533eaf7d1226bc4bf8b0196e086c94e8981
Miguel Angel Quintana <quintana.mige@gmail.com>
2017-03-03 00:47:53 -0800

    Replace space facts with cat facts and info on skill

---
+++ src/index.js
@@ -0,0 +1,122 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+var languageStrings = {
+    "en-GB": {
+        "translation": {
+            "FACTS": [
+                " Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+                "There are cats who have survived falls from over 32 stories (320 meters) onto concrete.",
+                "A group of cats is called a clowder.",
+                "Cats have over 20 muscles that control their ears.",
+                "Cats sleep 70% of their lives.",
+                "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs."
+                "And one ran for mayor of Mexico City in 2013.",
+                "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.",
+                "In tigers and tabbies, the middle of the tongue is covered in backward-pointing spines, used for breaking off and gripping meat.",
+                "When cats grimace, they are usually “taste-scenting.” They have an extra organ that, with some breathing control, allows the cats to taste-sense the air.",
+                " Cats can’t taste sweetness.",
+                "Unlike humans, cats only sweat through their paws. This is why you may see them leave moist paw prints in the summer time!",
+                "A female cat carries her kittens for about 58-65 days before they are born.",
+                "Cats have five toes on each front paw, but only four toes on each back paw.",
+                "Cats can run up to 30 miles per hour.",
+                "Cats have more than 100 different vocal sounds.",
+                "Cats knead with their paws when they are happy.",
+                "Calico cats are almost always female.",
+                "A male cat is called a 'Tom' and a female cat is called a 'Queen.'",
+                "The righting reflex has helped some cats survive falls from over 32 stories (320 meters) onto solid ground.",
+                "According to researchers, keeping a cat as a pet reduces the risk of heart attacks and strokes by nearly one third.",
+                "Adult cats never meow to each other but only to communicate with humans.",
+                "Cat cafes, where a person can drink coffee and hang out with cats, are becoming popular around the world.",
+                "In Japan, a black cat is considered a good luck charm.",
+                "The world record of producing the most of kittens is held by a cat called Dusty, who gave birth to 420 kittens during her breeding life.",
+                "Cats use their whiskers to figure out how wide an opening is and whether they will fit through it."
+                
+                
+            ],
+            "SKILL_NAME" : "Amazing Cat Facts",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "HELP_MESSAGE" : "You can say tell me a cat fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "Goodbye!"
+        }
+    },
+    "en-US": {
+        "translation": {
+            "FACTS": [
+                " Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+                "There are cats who have survived falls from over 32 stories (320 meters) onto concrete.",
+                "A group of cats is called a clowder.",
+                "Cats have over 20 muscles that control their ears.",
+                "Cats sleep 70% of their lives.",
+                "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs."
+                "And one ran for mayor of Mexico City in 2013.",
+                "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.",
+                "In tigers and tabbies, the middle of the tongue is covered in backward-pointing spines, used for breaking off and gripping meat.",
+                "When cats grimace, they are usually “taste-scenting.” They have an extra organ that, with some breathing control, allows the cats to taste-sense the air.",
+                " Cats can’t taste sweetness.",
+                "Unlike humans, cats only sweat through their paws. This is why you may see them leave moist paw prints in the summer time!",
+                "A female cat carries her kittens for about 58-65 days before they are born.",
+                "Cats have five toes on each front paw, but only four toes on each back paw.",
+                "Cats can run up to 30 miles per hour.",
+                "Cats have more than 100 different vocal sounds.",
+                "Cats knead with their paws when they are happy.",
+                "Calico cats are almost always female.",
+                "A male cat is called a 'Tom' and a female cat is called a 'Queen.'",
+                "The righting reflex has helped some cats survive falls from over 32 stories (320 meters) onto solid ground.",
+                "According to researchers, keeping a cat as a pet reduces the risk of heart attacks and strokes by nearly one third.",
+                "Adult cats never meow to each other but only to communicate with humans.",
+                "Cat cafes, where a person can drink coffee and hang out with cats, are becoming popular around the world.",
+                "In Japan, a black cat is considered a good luck charm.",
+                "The world record of producing the most of kittens is held by a cat called Dusty, who gave birth to 420 kittens during her breeding life.",
+                "Cats use their whiskers to figure out how wide an opening is and whether they will fit through it."
+            ],
+            "SKILL_NAME" : " Amazing Cat Facts",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "HELP_MESSAGE" : "You can say tell me a cat fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "Goodbye!"
+        }
+    }
+};
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random cat fact from the cat facts list
+        // Use this.t() to get corresponding language data
+        var factArr = this.t('FACTS');
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+
+        // Create speech output
+        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
+        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = this.t("HELP_MESSAGE");
+        var reprompt = this.t("HELP_MESSAGE");
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+};
